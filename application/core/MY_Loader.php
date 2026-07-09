da6e942e5ffa9bf66dc5d1bba36ee6d95e4e0e39
Aeindray2722 <aeindraylay2722@gmail.com>
2026-07-09 16:12:09 +0630

    fix My_Loader

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -23,6 +23,7 @@ class MY_Loader extends CI_Loader
         $CI->$property = new $service();
     }
 
+
     /**
      * Load Repository
      */
@@ -43,6 +44,7 @@ class MY_Loader extends CI_Loader
         $CI->$property = new $repository();
     }
 
+
     /**
      * Load Request
      */
@@ -62,50 +64,4 @@ class MY_Loader extends CI_Loader
 
         $CI->$property = new $request();
     }
-
-class MY_Loader extends CI_Loader
-{
-
-    public function service($service)
-    {
-
-        $file = APPPATH . 'services/' . $service . '.php';
-
-
-        if(!file_exists($file))
-        {
-            show_error(
-                "Service file not found: ".$file
-            );
-        }
-
-
-        require_once($file);
-
-
-        if(!class_exists($service))
-        {
-            show_error(
-                "Service class not found: ".$service
-            );
-        }
-
-
-        $CI =& get_instance();
-
-
-        // Convert:
-        // LookupGroupService
-        //
-        // to:
-        // lookupgroupservice
-
-        $property = strtolower($service);
-
-
-
-        $CI->$property = new $service();
-
-    }
-
 }
